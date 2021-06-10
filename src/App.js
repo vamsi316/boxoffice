0314d0439bcb29546c7f92989abc97a5d48ccab3
jellyfish <jvamsif223@gmail.com>
2021-06-11 01:54:16 +0530

    added layout management and created title

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
-import Navs from './components/Navs'
 import Home from './pages/Home'
 import Starred from './pages/Starred'
 
 function App() {
-  return <div>
-    
-    <Navs />
+  return (
 
     <Switch>
 
-    <Route exact path="/">
-      <Home />
-    </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
 
-    <Route exact path="/starred">
-      <Starred />
-    </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
 
-    <Route>
-      <div>Not Found</div>
-    </Route>
+      <Route>
+        <div>Not Found</div>
+      </Route>
   </Switch>
-  </div>;
+  );
 }
 
 export default App;
