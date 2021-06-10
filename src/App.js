2e60a6e55a9f97e57e49d75081219c4591a6f51c
jellyfish <jvamsif223@gmail.com>
2021-06-11 01:15:01 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
-  return (
-    <div>
-      hi
-    </div>
-  );
+  return <Switch>
+
+    <Route exact path="/">
+      This is home page
+    </Route>
+
+    <Route exact path="/starred">
+      This is starred page
+    </Route>
+
+    <Route>
+      This is 404 page
+    </Route>
+  </Switch>;
 }
 
 export default App;
