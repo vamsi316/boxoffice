75d94b58f837722c41ffdfa9c541eac3430c0677
jellyfish <jvamsif223@gmail.com>
2021-06-11 19:08:27 +0530

    styled show page

--- src/components/MainPageLayout.js
+++ src/components/MainPageLayout.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Navs from './Navs';
 import Title from './Title';
 
-function MainPageLayout({ children }) {
+const MainPageLayout = ({ children }) => {
   return (
     <div>
-        <Title title="Box Office" subtitle="Are you looking for a movie or an actor"/>
-        <Navs />
+      <Title
+        title="Box Office"
+        subtitle="Are you looking for a movie or an actor?"
+      />
+      <Navs />
 
-        {children}
+      {children}
     </div>
   );
-}
+};
 
 export default MainPageLayout;
