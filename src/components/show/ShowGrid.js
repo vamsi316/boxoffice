960089293eced27b6d16cacd19e0c32e401b7a75
jellyfish <jvamsif223@gmail.com>
2021-06-11 21:16:42 +0530

    finished starred page

--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -4,30 +4,26 @@ import ShowCard from './ShowCard';
 import { FlexGrid } from '../styled';
 
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
-import {useShows} from '../../misc/custom-hooks';
+import { useShows } from '../../misc/custom-hooks';
 
 const ShowGrid = ({ data }) => {
-
-  const [starredShows, dispatchStarred] = useShows()
+  const [starredShows, dispatchStarred] = useShows();
 
   return (
     <FlexGrid>
       {data.map(({ show }) => {
-          
-          const isStarred = starredShows.includes(show.id);
+        const isStarred = starredShows.includes(show.id);
 
-          const onStarClick = () => {
-
-            if(isStarred) {
-              dispatchStarred({type: 'REMOVE', showId: show.id});
-            }
-            else {
-              dispatchStarred({type: 'ADD', showId: show.id});
-            }
+        const onStarClick = () => {
+          if (isStarred) {
+            dispatchStarred({ type: 'REMOVE', showId: show.id });
+          } else {
+            dispatchStarred({ type: 'ADD', showId: show.id });
           }
+        };
 
-          return (
-            <ShowCard
+        return (
+          <ShowCard
             key={show.id}
             id={show.id}
             name={show.name}
@@ -35,8 +31,8 @@ const ShowGrid = ({ data }) => {
             summary={show.summary}
             onStarClick={onStarClick}
             isStarred={isStarred}
-            />
-          );
+          />
+        );
       })}
     </FlexGrid>
   );
